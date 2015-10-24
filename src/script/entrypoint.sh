717dc4f573286bc224325b9dd087eb212236e9b0
Ian Bytchek <ianbytchek@gmail.com>
2015-10-25 01:07:23 +0400

    Introducting multi-configuration recognition

--- src/script/entrypoint.sh
+++ src/script/entrypoint.sh
@@ -1,26 +1,43 @@
 #!/usr/bin/env sh
 
 set -euo pipefail
-cd $(dirname $0)
+cd "$(dirname $0)"
 
-if [ $1 = 'haproxy' ]; then
+if [ "${1}" == 'haproxy' ]; then
 
     # If we have haproxy.cfg available in our configuration directory, then we use it, otherwise use the standard haproxy.cfg.
 
-    if [ -f '/etc/haproxy/haproxy.cfg' ]; then
-        haproxy -f '/etc/haproxy/haproxy.cfg'
-    else
-        cat <<-EOF
+    configuration_path='/etc/haproxy/haproxy.cfg'
 
-			****************************************************************************************************
-
-			    It looks like you haven't specified the haproxy.cfg file, the default one will be used, meaning
-			    the HAProxy won't do anything useful. See the README.md that came with the Dockerfile.
-
-			****************************************************************************************************
-
-		EOF
-
-        exit 1
+    if [ -f "${configuration_path}" ]; then
+        echo "Starting haproxy with default configuration, ${configuration_path}."
+        haproxy -f "${configuration_path}"
     fi
+
+    configuration_command=''
+    configuration_paths=''
+
+    for file in '/etc/haproxy/'*'.cfg'; do
+        [ "${#configuration_paths}" > 0 ] && configuration_paths+=', '
+        configuration_paths+="${file}"
+        configuration_command+=" -f ${file}"
+    done
+
+    if [ -n "${configuration_command}" ]; then
+        echo "Starting haproxy with multiple configurations, ${configuration_paths}."
+        eval "haproxy ${configuration_command}"
+    fi
+
+    cat <<-EOF
+
+		****************************************************************************************************
+
+		    You haven't provided any configuration files, see the README.md that came with the Dockerfile
+		    to find out how to do that.
+
+		****************************************************************************************************
+
+	EOF
+
+    exit 1
 fi
